bfde4bbf0fc1e47b91896e21f7d4dbd5a028f820
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-16 22:12:37 +0900

    Initial Commit

---
+++ RandomGetMoney/RandomGetMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomGetMoneyApp.swift
+//  RandomGetMoney
+//
+//  Created by 髙木文弘 on 2021/03/16.
+//
+
+import SwiftUI
+
+@main
+struct RandomGetMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomGetMoneyTests/RandomGetMoneyTests.swift
@@ -0,0 +1,33 @@
+//
+//  RandomGetMoneyTests.swift
+//  RandomGetMoneyTests
+//
+//  Created by 髙木文弘 on 2021/03/16.
+//
+
+import XCTest
+@testable import RandomGetMoney
+
+class RandomGetMoneyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
